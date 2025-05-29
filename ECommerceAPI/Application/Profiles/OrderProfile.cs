9ffcb2addcb7ab303b5858706530aca625659143
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-05-28 23:38:44 -0500

    Enhance OrderProfile mapping to include UserId in OrderResponse and update OrderProfile for OrderDate mapping.

--- ECommerceAPI/Application/Profiles/OrderProfile.cs
+++ ECommerceAPI/Application/Profiles/OrderProfile.cs
@@ -13,12 +13,13 @@ namespace ECommerceAPI.Application.Profiles
             CreateMap<OrderCreateRequest, Order>()
                 //.ForMember(dest => dest.OrderStatus, opt => opt.MapFrom(src => "Pending"))
                 .ForMember(dest => dest.OrderDate, opt => opt.MapFrom(src => DateTime.UtcNow));
-            
+
             CreateMap<OrderProductRequest, OrderProduct>();
             CreateMap<OrderEntity, Order>().ReverseMap();
             CreateMap<OrderProductEntity, OrderProduct>().ReverseMap();
             CreateMap<OrderProduct, OrderProductResponse>();
-            CreateMap<Order, OrderResponse>();
+            CreateMap<Order, OrderResponse>()
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId));
         }
     }
-}   
+}
--- ECommerceAPI/WebApi/DTOs/ResponseModels/OrderResponse.cs
+++ ECommerceAPI/WebApi/DTOs/ResponseModels/OrderResponse.cs
@@ -6,6 +6,7 @@ namespace ECommerceAPI.WebApi.DTOs.ResponseModels
     public class OrderResponse
     {
         public int Id { get; set; }
+        public int UserId { get; set; }
         public List<OrderProductResponse> OrderProducts { get; set; }
         public decimal TotalAmount { get; set; }
         public string PaymentStatus { get; set; }
